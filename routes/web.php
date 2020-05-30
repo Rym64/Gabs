c48c89fb39df60516be875dd9b317a1eae782d9a
Rebii Rym <rymrebii@gmail.com>
2020-05-30 19:19:30 +0200

    lastproject

---
+++ routes/web.php
@@ -0,0 +1,133 @@
+<?php
+use App\User;
+use Illuminate\Support\Facades\Input;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+//////////////////////////////////////////////////////////////
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'AppController@index')->name('welcome');
+Route::get('/profile', 'UserController@profile')->name('profile');
+Route::post('/profile', 'UserController@update_image');
+
+
+/********************** Search absense**************************/
+
+Route::post('/search',function(){
+  $q = Input::get ( 'q' );
+  $user = User::where('role',"student")->where('name','LIKE','%'.$q.'%')->get();
+  if(count($user) > 0)
+      return view('teacher.abscense.gestionabs')->withDetails($user)->withQuery ( $q );
+  else return view ('teacher.abscense.gestionabs')->withMessage('No Details found. Try to search again !');
+});
+
+
+
+/********************** Search seance **************************/
+Route::GET('/searchseance',function(){
+  $seance = Input::get ( 'seance' );
+  $user = User::where('date', '%Y-%m-%d','LIKE','%'.$seance.'%')->where('Heure_debut', '%Y-%m-%d','LIKE', '%'.$seance.'%')->where('Heure_fin', '%Y-%m-%d','LIKE','%'.$seance.'%')->get();
+  if(count($seance) > 0)
+      return view('teacher.abscense.affectabsence')->withDetails($seance)->withQuery ( $seance );
+  else return view ('teacher.abscense.affectabsence')->withMessage('No Details found. Try to search again !');
+});
+
+Auth::routes();
+
+
+Route::middleware('auth')->group(function () {
+
+
+  
+    Route::get('/home', 'AppController@home')->name('home');
+
+  
+    Route::get('admin/affectclasse/{user}', 'Admin\UserController@affectclasse');
+
+    Route::patch('admin/cls/{classe}', 'Admin\UserController@cls');
+
+
+    Route::patch('admin/updateclasse/{user}', 'Admin\UserController@updateclasse');
+
+    Route::get('admin/destroyteacher/{user}', 'Admin\UserController@destroyteacher');
+
+    Route::get('admin/editteacher/{user}', 'Admin\UserController@editteacher');
+
+    Route::patch('admin/updateteacher/{user}', 'Admin\UserController@updateteacher');
+
+    Route::patch('admin/updatemodul/{user}', 'Admin\UserController@updatemodul');
+    Route::get('admin/showteacher/{user}', 'Admin\UserController@showteacher');
+    Route::patch('admin/updatemodulteacher/{user}', 'Admin\UserController@updatemodulteacher');
+
+    Route::patch('admin/updateclasses/{classe}', 'Admin\ClasseController@updateclasses');
+
+    Route::get('teacher/affichemodul', 'Teacher\UserController@affichemodul');
+    
+    Route::post('teacher/affichestudent', 'Teacher\ClasseController@affichestudent');
+
+    Route::get('teacher/affichemodulse', 'Teacher\SeanceController@affichemodulse');
+    //Route::get('teacher/chercherseance', 'Teacher\SeanceController@chercherseance');
+    //Route::get('teacher/chercherstudent', 'Teacher\UserController@chercherstudent');
+
+
+    Route::group([
+      'middleware' => 'teacher',
+      'namespace' => 'Teacher',
+      'prefix' => 'teacher'
+    ], function () {
+
+        Route::resource('seance', 'SeanceController');
+      
+  });
+
+
+    // 
+    Route::group([
+                'middleware' => 'admin',
+                'namespace' => 'Admin',
+                'prefix' => 'admin'
+            ], function () {
+
+                
+                  
+                    Route::resource('modul', 'ModulController');
+                
+                  
+                  Route::get('indexteacher', 'UserController@indexteacher');
+                  Route::get('editteacher', 'UserController@editteacher');
+                  Route::get('affect', 'UserController@affect');
+            
+                  Route::get('createuser', 'UserController@createuser');
+
+          Route::resource('users', 'UserController');
+
+          
+          Route::resource('classe', 'ClasseController');
+
+  
+         
+       
+       
+            });
+});
